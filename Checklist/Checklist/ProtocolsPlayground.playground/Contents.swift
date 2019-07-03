d22ad4fbb848b1a868526c51ba0c5e391c1bbfb5
jelly <chm073@gmail.com>
2019-07-03 20:35:56 +0900

    Complete Protocols

---
+++ Checklist/Checklist/ProtocolsPlayground.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+protocol Persist {
+    func save()
+}
+
+class Monster: Persist {
+    func save() {
+        print("Moster save")
+    }
+    
+}
+
+
+class Sword: Persist {
+    func save() {
+        print("Sword save")
+    }
+}
+
+class Player {
+    
+}
+
+let monster = Monster()
+let sword = Sword()
+let player = Player()
+
+
+let items: [Persist] = [monster, sword]
+
+
+class GameManager {
+    func saveLevel(_ items: [Persist]) {
+        for item in items {
+            item.save()
+        }
+    }
+}
+
+let gameManager = GameManager()
+gameManager.saveLevel(items)
