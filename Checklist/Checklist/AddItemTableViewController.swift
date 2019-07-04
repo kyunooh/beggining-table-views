b30b424e4389dc6ad0d187ade2c0b55fdbf19152
Hyeon-Mook Jerry Choi <chm073@gmail.com>
2019-07-05 00:04:39 +0900

    Complete part3

--- Checklist/Checklist/AddItemTableViewController.swift
+++ Checklist/Checklist/AddItemTableViewController.swift
@@ -9,23 +9,56 @@
 import UIKit
 
 class AddItemTableViewController: UITableViewController {
-
+    
     @IBOutlet weak var textField: UITextField!
     @IBAction func cancel(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
     @IBAction func done(_ sender: Any) {
-                navigationController?.popViewController(animated: true)
+        navigationController?.popViewController(animated: true)
+        print("Contents of textField\(textField.text)")
     }
+        
+    @IBOutlet weak var cancelBarButton: UIBarButtonItem!
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        textField.becomeFirstResponder()
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+}
 
-
+extension AddItemTableViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let oldText = textField.text,
+              let stringRange = Range(range, in: oldText) else {
+                return false
+        }
+        
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        if newText.isEmpty {
+            addBarButton.isEnabled = false
+        } else {
+            addBarButton.isEnabled = true
+        }
+        return true
+    }
 }
